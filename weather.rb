98a7c9c3f124fa0e4255351a2ce33041c9cc5941
yosshi <yosshi1202@gmail.com>
2010-03-30 00:34:51 +0900

    release weather.rb

--- weather.rb
+++ weather.rb
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
+$KCODE='u'
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 require 'uri'
 require 'kconv'
-class Weather < Citrus::Plugin
+
+class Weather < SkypeBot::Plugin
   def initialize(*args)
     super
     @prefix = @config['prefix'] || 'の天気'
@@ -12,43 +14,33 @@ class Weather < Citrus::Plugin
   end
 
   def on_privmsg(prefix, channel, message)
-    return if !@config["channels"].nil? && !@config["channels"].include?(channel)
-
     case message
-    when /^[\s]{0,}#{@prefix}/i
+    when /^[\s]{0,}(.+)#{@prefix}/i
       day = %w(今日 明日 明後日 明々後日)
       weather,censer = getWeather(Regexp.last_match[1])
+      #weather,censer = getWeather("東京")
       weather.each_with_index do |m,i|
-        notice (channel, "#{day[i]}#{m} (#{censer[i]})")
+        sendMessage(channel, "#{day[i]}#{m} (#{censer[i]})")
       end
     end
   end
 
   private
   def getWeather(city)
-    
+
     h = Nokogiri::HTML(Kconv.toutf8(open(URI.escape("http://www.google.co.jp/search?q=週間天気+#{city}")).read))
 
     weather = []
     h.search("img").each{ |i|
       weather << "の天気は#{i["title"]}" unless i["title"].nil?
     }
-    
 
     censer = []
     h.search("table.ts.std div nobr").each{ |n|
-      censer << n.inner_text.gsub(/°C/,"度") unless n.inner_text.blank?
+      censer << n.inner_text.gsub(/°C/,"度") unless n.inner_text.empty?
     }
 
     [weather,censer]
   end
 
-
 end
-
-
-
-
-
-
-
